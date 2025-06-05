3f0a04be82d9dcefe1ce166f4bd9ef95754c4f46
dcronkite <dcronkite@gmail.com>
2025-06-05 12:43:15 -0700

    Added testing for logging output

--- test/test_run4snippets.py
+++ test/test_run4snippets.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from konsepy.run4snippets import run4snippets
 
 
-def test_run4snippets(tmp_path, request):
+def test_run4snippets(tmp_path, request, caplog):
     input_file = Path(request.fspath).parent / 'data' / 'corpus.jsonl'
     outdir = run4snippets(
         [input_file],
@@ -13,6 +13,13 @@ def test_run4snippets(tmp_path, request):
         id_label='chapter',
         noteid_label='chapter',
     )
+    # test log results
+    loglines = caplog.text.split('\n')
+    assert 'Arguments ignored: {}' in loglines[0]
+    assert 'Loaded 3 concepts for processing' in loglines[1]
+    assert 'Total records: 117' in loglines[2]
+
+    # test jsonlines output
     exp_keys = {'note_id', 'concept', 'category', 'studyid', 'note_date', 'match', 'start_index',
                 'end_index', 'precontext', 'postcontext', 'pretext', 'posttext'}
     with open(outdir / 'output.jsonl', encoding='utf8') as fh:
