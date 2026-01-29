d90aefef94dba5c665482a7e82ccd0f626964560
dcronkite <dcronkite@gmail.com>
2026-01-29 15:21:02 -0800

    Fix issue with caplog not capturing loguru logging output

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,17 @@
+import logging
 import pytest
+from loguru import logger
+
+
+@pytest.fixture
+def caplog(caplog):
+    class PropagateHandler(logging.Handler):
+        def emit(self, record):
+            logging.getLogger(record.name).handle(record)
+
+    handler_id = logger.add(PropagateHandler(), format="{message}")
+    yield caplog
+    logger.remove(handler_id)
 
 
 @pytest.fixture
